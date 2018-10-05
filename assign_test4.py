37e66c99b803e4f07ec414afe4cd41dd78978f42
Natalie <nrb75@cornell.edu>
2018-10-04 21:44:20 -0400

    up

--- assign_test4.py
+++ assign_test4.py
@@ -40,5 +40,5 @@ def assign_servers_test_output(df_train, df_test, percentile, confidence, apps_s
     server_df, server_assignments, total_latency, total_latency_model, avg_latency, avg_latency_model = server_association(formated_rules, df_test, apps_server) #this function loaded fr
 
     #return(formated_rules)
-    return(server_assignments, total_latency, total_latency_model, avg_latency, avg_latency_model)  
+    return(server_df, server_assignments, total_latency, total_latency_model, avg_latency, avg_latency_model)  
   
